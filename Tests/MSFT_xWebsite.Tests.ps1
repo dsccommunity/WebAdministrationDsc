dbb876927a81de17eb10560546ad3216fe4f5d81
Paul Wiens <pwiens@microsoft.com>
2015-05-22 13:42:17 -0700

    Adding IIS

--- Tests/MSFT_xWebsite.Tests.ps1
+++ Tests/MSFT_xWebsite.Tests.ps1
@@ -21,7 +21,7 @@ $testsitePhyicalPath = "C:\inetpub\test"
 # should check for the server OS
 if($env:APPVEYOR_BUILD_VERSION)
 {
-  #Add-WindowsFeature RSAT-DHCP -verbose
+    Add-WindowsFeature Web-Server -verbose
 }
 
 function Suite.BeforeAll {
@@ -32,72 +32,72 @@ function Suite.BeforeAll {
 function Suite.AfterAll {
     Remove-Module MSFT_xWebsite
     $global:WebsiteCertificateTest=$null
-	$personalCertPath = (Join-Path Cert:\LocalMachine\Personal\ -ChildPath $personalCert.Thumbprint)
-	$webhostCertPath = (Join-Path Cert:\LocalMachine\WebHosting\ -ChildPath $personalCert.Thumbprint)
-	if (Test-Path $personalCertPath){
-		Remove-Item $personalCertPath
-	}
-	if (Test-Path $webhostCertPath){
-		Remove-Item $webhostCertPath
-	}
+    $personalCertPath = (Join-Path Cert:\LocalMachine\Personal\ -ChildPath $personalCert.Thumbprint)
+    $webhostCertPath = (Join-Path Cert:\LocalMachine\WebHosting\ -ChildPath $personalCert.Thumbprint)
+    if (Test-Path $personalCertPath){
+        Remove-Item $personalCertPath
+    }
+    if (Test-Path $webhostCertPath){
+        Remove-Item $webhostCertPath
+    }
 }
 
 try
 {
     Describe 'Validate-ResourceProperties' {
-		AfterEach {
-			Remove-Website -Name "TestWebSite"
-			if (Test-Path $testsitePhyicalPath) {
-				Remove-Item $testsitePhyicalPath
-			}
+        AfterEach {
+            Remove-Website -Name "TestWebSite"
+            if (Test-Path $testsitePhyicalPath) {
+                Remove-Item $testsitePhyicalPath
+            }
         }
         It 'Binds to the correct web hosting certificate' {
-			if (-Not (Test-Path $testsitePhyicalPath)){
-				New-Item $testsitePhyicalPath -ItemType Directory
-			}
-			Import-PfxCertificate -FilePath $PSScriptRoot\webhosting.pfx -CertStoreLocation Cert:\LocalMachine\WebHosting
-			$testSite = @{
-						"Name" = "TestWebSite";
-						"Ensure" = "Present";
-						"State" = "Started";
-						"PhysicalPath" = $testsitePhyicalPath;
-						"ApplicationPool" = "DefaultAppPool";
-						"BindingInfo" = @(New-CimInstance -ClassName MSFT_xWebBindingInformation -Namespace root/microsoft/Windows/DesiredStateConfiguration -Property @{
-											Protocol = "https";
-											IPAddress = "*";
-											Port = 11443;
-											HostName = "localhost";
-											CertificateThumbprint = $webhostCert.Thumbprint;
-											CertificateStoreName = "WebHosting"} -ClientOnly);
-						"DefaultPage" = "index.htm"
-						}
-			Set-TargetResource @testSite
+            if (-Not (Test-Path $testsitePhyicalPath)){
+                New-Item $testsitePhyicalPath -ItemType Directory
+            }
+            Import-PfxCertificate -FilePath $PSScriptRoot\webhosting.pfx -CertStoreLocation Cert:\LocalMachine\WebHosting
+            $testSite = @{
+                        "Name" = "TestWebSite";
+                        "Ensure" = "Present";
+                        "State" = "Started";
+                        "PhysicalPath" = $testsitePhyicalPath;
+                        "ApplicationPool" = "DefaultAppPool";
+                        "BindingInfo" = @(New-CimInstance -ClassName MSFT_xWebBindingInformation -Namespace root/microsoft/Windows/DesiredStateConfiguration -Property @{
+                                            Protocol = "https";
+                                            IPAddress = "*";
+                                            Port = 11443;
+                                            HostName = "localhost";
+                                            CertificateThumbprint = $webhostCert.Thumbprint;
+                                            CertificateStoreName = "WebHosting"} -ClientOnly);
+                        "DefaultPage" = "index.htm"
+                        }
+            Set-TargetResource @testSite -Debug
 
-			Test-TargetResource @testSite | Should Be "True"
+            Test-TargetResource @testSite | Should Be "True"
         }
-		It 'Binds to the correct personal certificate' {
-			if (-Not (Test-Path $testsitePhyicalPath)){
-				New-Item $testsitePhyicalPath -ItemType Directory
-			}
-			Import-PfxCertificate -FilePath $PSScriptRoot\personal.pfx -CertStoreLocation Cert:\LocalMachine\Personal
-			$testSite = @{
-						"Name" = "TestWebSite";
-						"Ensure" = "Present";
-						"State" = "Started";
-						"PhysicalPath" = $testsitePhyicalPath;
-						"ApplicationPool" = "DefaultAppPool";
-						"BindingInfo" = @(New-CimInstance -ClassName MSFT_xWebBindingInformation -Namespace root/microsoft/Windows/DesiredStateConfiguration -Property @{
-											Protocol = "https";
-											IPAddress = "*";
-											Port = 11443;
-											HostName = "localhost";
-											CertificateThumbprint = $personalCert.Thumbprint;
-											CertificateStoreName = "Personal"} -ClientOnly);
-						"DefaultPage" = "index.htm"
-						}
-			Set-TargetResource @testSite
+        It 'Binds to the correct personal certificate' {
+            if (-Not (Test-Path $testsitePhyicalPath)){
+                New-Item $testsitePhyicalPath -ItemType Directory
+            }
+            Import-PfxCertificate -FilePath $PSScriptRoot\personal.pfx -CertStoreLocation Cert:\LocalMachine\Personal
+            $testSite = @{
+                        "Name" = "TestWebSite";
+                        "Ensure" = "Present";
+                        "State" = "Started";
+                        "PhysicalPath" = $testsitePhyicalPath;
+                        "ApplicationPool" = "DefaultAppPool";
+                        "BindingInfo" = @(New-CimInstance -ClassName MSFT_xWebBindingInformation -Namespace root/microsoft/Windows/DesiredStateConfiguration -Property @{
+                                            Protocol = "https";
+                                            IPAddress = "*";
+                                            Port = 11443;
+                                            HostName = "localhost";
+                                            CertificateThumbprint = $personalCert.Thumbprint;
+                                            CertificateStoreName = "Personal"} -ClientOnly);
+                        "DefaultPage" = "index.htm"
+                        }
+            Set-TargetResource @testSite
 
-			Test-TargetResource @testSite | Should Be "True"
+            Test-TargetResource @testSite | Should Be "True"
         }
     }
 
