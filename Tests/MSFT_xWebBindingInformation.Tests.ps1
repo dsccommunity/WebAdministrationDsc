a4621a7c85f5c83ec740b1cc81b88fc2aa614e92
Paul Wiens <paul@dysonshell.com>
2015-05-23 11:53:37 -0700

    Added module import statement

--- Tests/MSFT_xWebBindingInformation.Tests.ps1
+++ Tests/MSFT_xWebBindingInformation.Tests.ps1
@@ -6,6 +6,8 @@ if($env:APPVEYOR_BUILD_VERSION)
   Add-WindowsFeature Web-Server -Verbose
 }
 
+Import-Module (Join-Path $here -ChildPath "..\DSCResources\MSFT_xWebsite\MSFT_xWebsite.psm1")
+
 Describe "MSFT_xWebBindingInformation" {
     $storeNames = (Get-CimClass -Namespace "root/microsoft/Windows/DesiredStateConfiguration" -ClassName "MSFT_xWebBindingInformation").CimClassProperties['CertificateStoreName'].Qualifiers['Values'].Value
     foreach ($storeName in $storeNames){
