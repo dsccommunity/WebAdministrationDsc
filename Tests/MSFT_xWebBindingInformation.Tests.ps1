0fca379a864f0bd7829208bdbf51fbb223cd048f
Paul Wiens <paul@dysonshell.com>
2015-05-23 11:41:26 -0700

    Adding web binding cert store tests

---
+++ Tests/MSFT_xWebBindingInformation.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# should check for the server OS
+if($env:APPVEYOR_BUILD_VERSION)
+{
+  Add-WindowsFeature Web-Server -Verbose
+}
+
+Describe "MSFT_xWebBindingInformation" {
+    $storeNames = (Get-CimClass -Namespace "root/microsoft/Windows/DesiredStateConfiguration" -ClassName "MSFT_xWebBindingInformation").CimClassProperties['CertificateStoreName'].Qualifiers['Values'].Value
+    foreach ($storeName in $storeNames){
+        It "Uses valid credential store: $storeName" {
+            (Join-Path -Path Cert:\LocalMachine -ChildPath $storeName) | Should Exist
+        }
+    }
+}
