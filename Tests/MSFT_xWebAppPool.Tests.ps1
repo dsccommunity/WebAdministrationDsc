d5eb0cda68b5bd2769b15436eb8b0502fdff8bbb
Aaron Zuehlke <azuehlke22+github@gmail.com>
2015-06-25 13:09:10 -0500

    added some cleanup in finally and removed mock Get-TargetResource since Test-TargetResource doesn't use it

--- Tests/MSFT_xWebAppPool.Tests.ps1
+++ Tests/MSFT_xWebAppPool.Tests.ps1
@@ -21,58 +21,63 @@ if (! (Get-Module WebAdministration))
 
 Describe "MSFT_xWebAppPool"{
     InModuleScope $ModuleName {
-        $apName = "PesterAppPool"
+        try
+        {
+            $apName = "PesterAppPool"
 
-        It 'Should pass Test-xDscResource Schema Validation' {
-            $result = Test-xDscResource MSFT_xWebAppPool
-            $result | Should Be $true
-        }
-
-        It 'Should create a new App Pool' {           
-            $testParams =@{
-                Name = $apName
-                Ensure = "Present"
+            It 'Should pass Test-xDscResource Schema Validation' {
+                $result = Test-xDscResource MSFT_xWebAppPool
+                $result | Should Be $true
             }
 
-            # if the app pool exists, remove it
-            if((Get-ChildItem IIS:\apppools).Name.Contains($apName))
-            {
-                Remove-WebAppPool -Name $apName -ErrorAction Stop
-            }
-            
-            Set-TargetResource @testParams
-            (Get-ChildItem IIS:\apppools).Name.Contains($apName) | Should be $true
-        }
-
-        It 'Passes test when App Pool does exist'{
-            $testParams =@{
-                Name = $apName
-                Ensure = "Present"
-            }
-
-            Mock -ModuleName $ModuleName Get-TargetResource { 
-                return @{ 
-                    Name = $testParams.Name
-                    Ensure = $testParams.Ensure
+            It 'Should create a new App Pool' {           
+                $testParams =@{
+                    Name = $apName
+                    Ensure = "Present"
                 }
+
+                # if the app pool exists, remove it
+                if((Get-ChildItem IIS:\apppools).Name.Contains($apName))
+                {
+                    Remove-WebAppPool -Name $apName -ErrorAction Stop
+                }
+
+                Set-TargetResource @testParams
+                (Get-ChildItem IIS:\apppools).Name.Contains($apName) | Should be $true
             }
 
-            Test-TargetResource @testParams | Should be $true
+            It 'Passes test when App Pool does exist'{
+                $testParams =@{
+                    Name = $apName
+                    Ensure = "Present"
+                }
+
+                Test-TargetResource @testParams | Should be $true
+            }
+
+            It 'Fails test when App Pool does not exist'{
+                $testParams =@{
+                    Name = $apName
+                    Ensure = "Present"
+                }
+
+                # if the app pool exists, remove it
+                if((Get-ChildItem IIS:\apppools).Name.Contains($apName))
+                {
+                    Remove-WebAppPool -Name $apName -ErrorAction Stop
+                }
+
+                Test-TargetResource @testParams | Should be $false
+            }
         }
-
-        It 'Fails test when App Pool does not exist'{
-            $testParams =@{
-                Name = $apName
-                Ensure = "Present"
-            }
-
+        finally
+        {
             # if the app pool exists, remove it
             if((Get-ChildItem IIS:\apppools).Name.Contains($apName))
             {
                 Remove-WebAppPool -Name $apName -ErrorAction Stop
             }
-
-            Test-TargetResource @testParams | Should be $false
         }
+        
     }
 }
