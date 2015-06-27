fb9dea2360b2e05560eae0ee61c9f206546fc038
xunilrj <xunilrj@hotmail.com>
2015-06-27 10:29:39 -0300

    Simple test that the webfarm exists and it is enabled

---
+++ DSCResources/MSFT_xWebfarm/MSFT_xWebfarm.psm1
@@ -0,0 +1,77 @@
+data LocalizedData
+{   
+}
+
+# The Get-TargetResource cmdlet is used to fetch the status of role or Website on the target machine.
+# It gives the Website info of the requested role/feature on the target machine.  
+function Get-TargetResource 
+{
+    [OutputType([System.Collections.Hashtable])]
+    param 
+    (   
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+    @{}
+}
+
+
+# The Set-TargetResource cmdlet is used to create, delete or configuure a website on the target machine. 
+function Set-TargetResource 
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param 
+    (       
+        [ValidateSet("Present", "Absent")]
+        [string]$Ensure = "Present",
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    ) 
+}
+
+
+# The Test-TargetResource cmdlet is used to validate if the role or feature is in a state as expected in the instance document.
+function Test-TargetResource 
+{
+    [OutputType([System.Boolean])]
+    param 
+    (       
+        [ValidateSet("Present", "Absent")]
+        [string]$Ensure = "Present",
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,       
+                
+        [bool]$Enabled = $true,
+
+        [string]$ConfigPath = "C:\Windows\System32\inetsrv\config\applicationHost.config"
+    )
+    
+    Write-Verbose "Searching for webfarm: $Name"
+
+    $found = $false    
+    $applicationHostConfig =[xml](gc $ConfigPath)
+    $measure = $applicationHostConfig.configuration.webFarms.webFarm | ? name -eq $Name | measure-object
+
+    Write-Verbose ("Webfarms found: " + $measure.Count)
+
+    if($measure.Count -gt 1){
+        Write-Error "More than one webfarm found! The config must be corrupted"
+    }elseif($measure.Count -eq 0){
+
+        $webfarmElement = ($applicationHostConfig.configuration.webFarms.webFarm | ? name -eq $Name)[0]
+        if($webfarmElement.enabled -ne $Enabled){
+            Write-Verbose "Webfarms is [$($webfarmElement.enabled)] but is requested to be [$Enabled]."
+            return $false
+        }
+
+    }
+
+    $measure.Count -gt 0     
+}
+
+#endregion
