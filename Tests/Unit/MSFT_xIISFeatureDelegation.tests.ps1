f15bebb26afef14b70841ac2e85cf66d63c38ee4
tysonjhayes <tysonh@microsoft.com>
2015-12-21 08:02:06 -0800

    Modifying to use template functions

--- Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
+++ Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
@@ -1,95 +1,27 @@
-<#
-.Synopsis
-   Template for creating Unit Tests
-.DESCRIPTION
-   To Use:
-     1. Copy to \Tests\Integration\ folder and rename MSFT_x<ResourceName>.tests.ps1
-     2. Customize TODO sections.
-     3. Create MSFT_x<ResourceName>.config.ps1 to be used as test DSC Configurtion file.
-
-.NOTES
-   Code in HEADER and FOOTER regions are standard and may be moved into DSCResource.Tools in
-   Future and therefore should not be altered if possible.
-#>
-
-
-# TODO: Customize these paramters...
-$DSCModuleName      = 'xWebAdministration'
-$DSCResourceName    = 'MSFT_xIisFeatureDelegation'
-$RelativeModulePath = "DSCResources\$DSCResourceName\$DSCResourceName.psm1"
-# /TODO
+$global:DSCModuleName = 'xWebAdministration'
+$global:DSCResourceName = 'MSFT_xIISFeatureDelegation'
 
 #region HEADER
-# Temp Working Folder - always gets remove on completion
-$WorkingFolder = Join-Path -Path $env:Temp -ChildPath $DSCResourceName
-
-# Copy to Program Files for WMF 4.0 Compatability as it can only find resources in a few known places.
-$moduleRoot = "${env:ProgramFiles}\WindowsPowerShell\Modules\$DSCModuleName"
-
-# If this module already exists in the Modules folder, make a copy of it in
-# the temporary folder so that it isn't accidentally used in this test.
-if(-not (Test-Path -Path $moduleRoot))
+if ( (-not (Test-Path -Path '.\DSCResource.Tests\')) -or `
+     (-not (Test-Path -Path '.\DSCResource.Tests\TestHelper.psm1')) )
 {
-    $null = New-Item -Path $moduleRoot -ItemType Directory
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git')
 }
 else
 {
-    # Copy the existing folder out to the temp directory to hold until the end of the run
-    # Delete the folder to remove the old files.
-    $tempLocation = Join-Path -Path $env:Temp -ChildPath $DSCModuleName
-    Copy-Item -Path $moduleRoot -Destination $tempLocation -Recurse -Force
-    Remove-Item -Path $moduleRoot -Recurse -Force
-    $null = New-Item -Path $moduleRoot -ItemType Directory
+    & git @('-C',(Join-Path -Path (Get-Location) -ChildPath '\DSCResource.Tests\'),'pull')
 }
-
-
-# Copy the module to be tested into the Module Root
-Copy-Item -Path $PSScriptRoot\..\..\* -Destination $moduleRoot -Recurse -Force -Exclude '.git'
-
-# Import the Module
-$Splat = @{
-    Path = $moduleRoot
-    ChildPath = $RelativeModulePath
-    Resolve = $true
-    ErrorAction = 'Stop'
-}
-$DSCModuleFile = Get-Item -Path (Join-Path @Splat)
-
-# Remove all copies of the module from memory so an old one is not used.
-if (Get-Module -Name $DSCModuleFile.BaseName -All)
-{
-    Get-Module -Name $DSCModuleFile.BaseName -All | Remove-Module
-}
-
-# Import the Module to test.
-Import-Module -Name $DSCModuleFile.FullName -Force
-
-<#
-  This is to fix a problem in AppVoyer where we have multiple copies of the resource
-  in two different folders. This should probably be adjusted to be smarter about how
-  it finds the resources.
-#>
-if (($env:PSModulePath).Split(';') -ccontains $pwd.Path)
-{
-    $script:tempPath = $env:PSModulePath
-    $env:PSModulePath = ($env:PSModulePath -split ';' | Where-Object {$_ -ne $pwd.path}) -join ';'
-}
-
-# Preserve and set the execution policy so that the DSC MOF can be created
-$executionPolicy = Get-ExecutionPolicy
-if ($executionPolicy -ne 'Unrestricted')
-{
-    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-    $rollbackExecution = $true
-}
+Import-Module .\DSCResource.Tests\TestHelper.psm1 -Force
+$TestEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $Global:DSCModuleName `
+    -DSCResourceName $Global:DSCResourceName `
+    -TestType Unit
 #endregion
 
 # Begin Testing
 try
 {
-
     #region Pester Tests
-
     InModuleScope $DSCResourceName {
 
         #region Function Get-TargetResource
@@ -173,33 +105,6 @@ try
 finally
 {
     #region FOOTER
-    # Set PSModulePath back to previous settings
-    if ($script:tempPath)
-    {
-        $env:PSModulePath = $script:tempPath
-    }
-
-    # Restore the Execution Policy
-    if ($rollbackExecution)
-    {
-        Set-ExecutionPolicy -ExecutionPolicy $executionPolicy -Force
-    }
-
-    # Cleanup Working Folder
-    if (Test-Path -Path $WorkingFolder)
-    {
-        Remove-Item -Path $WorkingFolder -Recurse -Force
-    }
-
-    # Clean up after the test completes.
-    Remove-Item -Path $moduleRoot -Recurse -Force
-
-    # Restore previous versions, if it exists.
-    if ($tempLocation)
-    {
-        $null = New-Item -Path $moduleRoot -ItemType Directory
-        Copy-Item -Path $tempLocation -Destination "${env:ProgramFiles}\WindowsPowerShell\Modules" -Recurse -Force
-        Remove-Item -Path $tempLocation -Recurse -Force
-    }
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
     #endregion
 }
