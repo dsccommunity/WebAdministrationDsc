9348d962e2f11f7a02fef43ca64a3dcdbe2969dc
Mariah <mbreakey@microsoft.com>
2016-05-03 16:29:16 -0700

    Update MSFT_xIISFeatureDelegation.tests.ps1

--- Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
+++ Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
@@ -2,23 +2,18 @@ $global:DSCModuleName = 'xWebAdministration'
 $global:DSCResourceName = 'MSFT_xIISFeatureDelegation'
 
 #region HEADER
-if (true) # downloaded from gallery
+
+[String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
+if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
+     (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
 }
-else #downloaded from git
+else
 {
-    [String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
-    if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-         (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
-    {
-        & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
-    }
-    else
-    {
-        & git @('-C',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'),'pull')
-    }
+    & git @('-C',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'),'pull')
 }
+
 
 
 Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
