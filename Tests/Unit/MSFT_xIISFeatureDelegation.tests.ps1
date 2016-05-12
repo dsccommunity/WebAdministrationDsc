df7c302a600d245f0ba618cd45dd19fccb5d1ff2
Mariah Breakey <mbreakey@microsoft.com>
2016-05-12 15:14:39 -0700

    adding conditional logic to get rid of git dependency

--- Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
+++ Tests/Unit/MSFT_xIISFeatureDelegation.tests.ps1
@@ -4,14 +4,30 @@ $global:DSCResourceName = 'MSFT_xIISFeatureDelegation'
 #region HEADER
 
 [String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
- if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-      (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
-{
-    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
+$repoSource = (Get-Module -Name $global:DSCModuleName -ListAvailable)
+
+if ($repoSource.RepositorySourceLocation.Host -eq 'www.powershellgallery.com') {
+    if ( -not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DscResourceTestHelper')) ) {
+        #install test folders from gallery
+        Save-Module -Name 'DscResourceTestHelper' -Path $moduleRoot
+    }
+
+    $testModuleVer = '0.2.0.0'
+    Import-Module (Join-Path -Path $moduleRoot -ChildPath "DscResourceTestHelper\$testModuleVer\TestHelper.psm1") -Force
+} 
+elseif ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DscResource.Tests'))) -or `
+        (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DscResource.Tests\DscResourceTestHelper'))) -or `
+       (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DscResource.Tests\DscResourceTestHelper\TestHelper.psm1'))) ) {
+    #clone test folders from gitHub
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DscResource.Tests\'))
+
+    Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DscResource.Tests\DscResourceTestHelper\TestHelper.psm1') -Force
+}
+else {
+    Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DscResource.Tests\DscResourceTestHelper\TestHelper.psm1') -Force
 }
 
 
-Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
 $TestEnvironment = Initialize-TestEnvironment `
     -DSCModuleName $Global:DSCModuleName `
     -DSCResourceName $Global:DSCResourceName `
