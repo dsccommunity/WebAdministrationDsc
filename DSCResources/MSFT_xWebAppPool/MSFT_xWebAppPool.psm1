0ac5f69c0ada169d2be35b46e8de2837996788cc
Mariah Breakey <mbreakey@microsoft.com>
2016-06-10 16:05:39 -0700

    trying invoking the string, since the string by itself didn't work

--- DSCResources/MSFT_xWebAppPool/MSFT_xWebAppPool.psm1
+++ DSCResources/MSFT_xWebAppPool/MSFT_xWebAppPool.psm1
@@ -150,7 +150,7 @@ function Get-TargetResource
         }
     ).ForEach(
         {
-            $returnValue.Add($_.Name, ('{0}.{1}' -f $appPool.ToString(), $_.Path))
+            $returnValue.Add($_.Name, $('{0}.{1}' -f $appPool.ToString(), $_.Path))
             #$returnValue.Add($_.Name, (Invoke-Expression -Command ('$appPool.{0}' -f $_.Path)))
         }
     )
