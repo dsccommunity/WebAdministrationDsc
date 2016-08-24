01534c36297aed5fdd21a5a8be4186d34ae5ed23
Mariah Breakey <mbreakey@microsoft.com>
2016-08-24 12:28:52 -0700

    adding integration tests

---
+++ Tests/Integration/MSFT_xIisModule.config.ps1
@@ -0,0 +1,18 @@
+﻿
+# Integration Test Config Template Version: 1.0.0
+configuration MSFT_xIisModule_config {
+
+    Import-DscResource -ModuleName 'xWebAdministration'
+    
+    Node Localhost {
+
+        xIisModule Integration_Test {
+            Path = 'c:\php\php-cgi.exe'
+            Name = 'TestModule'
+            RequestPath = Join-Path -Path (Get-Location) -ChildPath TestModule
+            Verb = @('Verb1','Verb2')
+            Ensure = 'Present'
+        }
+    }
+}
+
