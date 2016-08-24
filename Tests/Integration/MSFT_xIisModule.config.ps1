80479897c402be9f7487b7a9fe993fe34b54133b
Mariah Breakey <mbreakey@microsoft.com>
2016-08-24 12:57:54 -0700

    fixing typo

--- Tests/Integration/MSFT_xIisModule.config.ps1
+++ Tests/Integration/MSFT_xIisModule.config.ps1
@@ -1,6 +1,6 @@
 ﻿
 # Integration Test Config Template Version: 1.0.0
-configuration MSFT_xIisModule_config {
+configuration MSFT_xIisModule_Present {
 
     Import-DscResource -ModuleName 'xWebAdministration'
     
