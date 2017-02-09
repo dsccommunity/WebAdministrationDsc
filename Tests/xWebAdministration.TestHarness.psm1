ddcc45966483e0431001a6ea1e35bfc34d3d7bde
Tyson Hayes <tysonh@microsoft.com>
2017-02-08 17:25:50 -0800

    Modifying test harness to also run integration and default tests

--- Tests/xWebAdministration.TestHarness.psm1
+++ Tests/xWebAdministration.TestHarness.psm1
@@ -2,14 +2,16 @@ function Invoke-xWebAdministrationTest
 {
     param
     (
-        [Parameter(Mandatory = $false)]
-        [System.String] $TestResultsFile,
+        [Parameter()]
+        [System.String]
+        $TestResultsFile,
 
-        [Parameter(Mandatory = $false)]
-        [System.String] $DscTestsPath
+        [Parameter()]
+        [System.String]
+        $DscTestsPath
     )
 
-    Write-Verbose 'Commencing xWebAdministration unit tests'
+    Write-Verbose 'Commencing all xWebAdministration tests'
 
     $repoDir = Join-Path $PSScriptRoot '..' -Resolve
 
@@ -29,16 +31,36 @@ function Invoke-xWebAdministrationTest
     }
 
     Import-Module "$repoDir\xWebAdministration.psd1"
-
-    $versionsToTest = (Get-ChildItem (Join-Path $repoDir '\Tests\Unit\')).Name
-
     $testsToRun = @()
-    $versionsToTest | ForEach-Object {
+
+    # Helper tests
+    $helperTests = (Get-ChildItem (Join-Path $repoDir '\Tests\Helper\')).Name
+
+    $helperTests | ForEach-Object {
+        $testsToRun += @(@{
+                'Path' = "$repoDir\Tests\Helper\$_"
+            })
+    }
+
+    # Run Unit Tests
+    $unitTests = (Get-ChildItem (Join-Path $repoDir '\Tests\Unit\')).Name
+
+    $unitTests | ForEach-Object {
         $testsToRun += @(@{
                 'Path' = "$repoDir\Tests\Unit\$_"
             })
     }
 
+    # Integration Tests
+    $integrationTests = (Get-ChildItem -Path (Join-Path $repoDir '\Tests\Integration\') -Filter '*.Tests.ps1').Name
+
+    $integrationTests | ForEach-Object {
+        $testsToRun += @(@{
+                'Path' = "$repoDir\Tests\Integration\$_"
+            })
+    }
+
+    # DSC Common Tests
     if ($PSBoundParameters.ContainsKey('DscTestsPath') -eq $true)
     {
         $testsToRun += @{
@@ -46,7 +68,9 @@ function Invoke-xWebAdministrationTest
         }
     }
 
-    $results = Invoke-Pester -Script $testsToRun -CodeCoverage $testCoverageFiles -PassThru @testResultSettings
+    $results = Invoke-Pester -Script $testsToRun `
+        -CodeCoverage $testCoverageFiles `
+        -PassThru @testResultSettings
 
     return $results
 
