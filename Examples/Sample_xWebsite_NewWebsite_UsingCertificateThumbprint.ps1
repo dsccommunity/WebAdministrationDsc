77efdb14440547657a1df42ac8e5194f0cdb4ece
Sayak Naskar <sayaknaskar@gmail.com>
2019-10-01 21:59:28 +0530

    Update Sample_xWebsite_NewWebsite_UsingCertificateThumbprint.ps1

--- Examples/Sample_xWebsite_NewWebsite_UsingCertificateThumbprint.ps1
+++ Examples/Sample_xWebsite_NewWebsite_UsingCertificateThumbprint.ps1
@@ -1,3 +1,10 @@
+<#
+    .DESCRIPTION
+        While setting up IIS and stopping the default website is interesting, it isn’t quite useful yet.
+        After all, people typically use IIS to set up websites of their own with custom protocol and bindings.
+        Fortunately, using DSC, adding another website is as simple as using the File and xWebsite resources to copy the website content and configure the website.
+#>
+
 Configuration Sample_xWebsite_NewWebsite
 {
     param
