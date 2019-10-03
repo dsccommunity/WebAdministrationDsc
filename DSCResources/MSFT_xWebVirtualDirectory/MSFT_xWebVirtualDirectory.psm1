2cdc7ac35b1c7165bd9a5d7835dd53ee8300fdc9
olivier.miossec <olivier.miossec@metsys.fr>
2019-10-03 23:40:55 +0200

    Localizeddata MSFT_xWebVirtualDirectory

--- DSCResources/MSFT_xWebVirtualDirectory/MSFT_xWebVirtualDirectory.psm1
+++ DSCResources/MSFT_xWebVirtualDirectory/MSFT_xWebVirtualDirectory.psm1
@@ -4,21 +4,8 @@ $script:localizationModulePath = Join-Path -Path $script:modulesFolderPath -Chil
 
 Import-Module -Name (Join-Path -Path $script:localizationModulePath -ChildPath 'xWebAdministration.Common.psm1')
 
-# Localized messages
-data LocalizedData
-{
-    # culture="en-US"
-    ConvertFrom-StringData -StringData @'
-        VerboseGetTargetResource               = Get-TargetResource has been run.
-        VerboseSetTargetPhysicalPath           = Updating physical path for web virtual directory "{0}".
-        VerboseSetTargetCreateVirtualDirectory = Creating new Web Virtual Directory "{0}".
-        VerboseSetTargetRemoveVirtualDirectory = Removing existing Virtual Directory "{0}".
-        VerboseTestTargetFalse                 = Physical path "{0}" for web virtual directory "{1}" does not match desired state.
-        VerboseTestTargetTrue                  = Web virtual directory is in required state.
-        VerboseTestTargetAbsentTrue            = Web virtual directory "{0}" should be absent and is absent.
-'@
-
-}
+# Import Localization Strings
+$script:localizedData = Get-LocalizedData -ResourceName  'MSFT_xWebVirtualDirectory'
 
 function Get-TargetResource
 {
