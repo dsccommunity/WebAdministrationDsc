33d8cb134963facee11378db53e8028d61261d14
olivier.miossec <olivier.miossec@metsys.fr>
2019-10-04 14:21:03 +0200

    MSFT_xWebConfigKeyValue localization data

--- DSCResources/MSFT_xWebConfigKeyValue/MSFT_xWebConfigKeyValue.psm1
+++ DSCResources/MSFT_xWebConfigKeyValue/MSFT_xWebConfigKeyValue.psm1
@@ -4,24 +4,8 @@ $script:localizationModulePath = Join-Path -Path $script:modulesFolderPath -Chil
 
 Import-Module -Name (Join-Path -Path $script:localizationModulePath -ChildPath 'xWebAdministration.Common.psm1')
 
-# Localized messages
-data LocalizedData
-{
-    # culture="en-US"
-    ConvertFrom-StringData -StringData @'
-    VerboseGetTargetCheckingTarget  = Checking for the existance of key "{0}" in ConfigSection "{1}" located at "{2}"
-    VerboseGetTargetAttributeCheck  = Checking if key "{0}" is an Attribute
-    VerboseGetTargetKeyNotFound     = Key "{0}" has not been found.
-    VerboseGetTargetKeyFound        = Key "{0}" has been found
-    VerboseSetTargetCheckingKey     = Checking for existance of key "{0}"
-    VerboseSetTargetAddItem         = Key "{0}" does not exist, adding key
-    VerboseSetTargetEditItem        = Key "{0}" exists, editing key
-    VerboseSetTargetRemoveItem      = Key "{0}" exists, removing key
-    VerboseTestTargetCheckingTarget = Checking for the existance of key "{0}" in ConfigSection "{1}" located at "{2}"
-    VerboseTestTargetKeyNotFound    = Key "{0}" has not been found.
-    VerboseTestTargetKeyWasFound    = Key "{0}" has been found.
-'@
-}
+# Import Localization Strings
+$script:localizedData = Get-LocalizedData -ResourceName 'MSFT_xWebConfigKeyValue'
 
 <#
     .SYNOPSIS
