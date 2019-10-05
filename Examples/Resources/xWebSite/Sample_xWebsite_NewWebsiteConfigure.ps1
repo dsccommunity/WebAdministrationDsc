f3d8b104fd8b1f30f701f45ca2e8e9495460fb6b
Sayak Naskar <sayaknaskar@gmail.com>
2019-10-05 11:12:19 +0530

    xWebSite: Moved example from README.md to examples folder with the name NewWebsiteConfigure.ps1 #487

--- Examples/Resources/xWebSite/Sample_xWebsite_NewWebsiteConfigure.ps1
+++ Examples/Resources/xWebSite/Sample_xWebsite_NewWebsiteConfigure.ps1
@@ -1,86 +1,79 @@
-configuration Sample_xWebsite_NewWebsite
+Configuration Sample_xWebsite_NewWebsite
 {
     param
     (
         # Target nodes to apply the configuration
-        [String[]] 
-        $NodeName = 'localhost',
-
+        [string[]]$NodeName = 'localhost',
         # Name of the website to create
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String] 
-        $WebSiteName,
-
-        # Optional Site Id for the website
-        [Parameter()]
-        [UInt32] $SiteId,
-
+        [String]$WebSiteName,
         # Source Path for Website content
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String] 
-        $SourcePath,
-
+        [String]$SourcePath,
         # Destination path for Website content
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String] 
-        $DestinationPath
+        [String]$DestinationPath
     )
 
     # Import the module that defines custom resources
-    Import-DscResource -Module xWebAdministration, PSDesiredStateConfiguration
-
+    Import-DscResource -Module xWebAdministration
     Node $NodeName
     {
         # Install the IIS role
         WindowsFeature IIS
         {
-            Ensure          = 'Present'
-            Name            = 'Web-Server'
+            Ensure          = "Present"
+            Name            = "Web-Server"
         }
 
         # Install the ASP .NET 4.5 role
         WindowsFeature AspNet45
         {
-            Ensure          = 'Present'
-            Name            = 'Web-Asp-Net45'
+            Ensure          = "Present"
+            Name            = "Web-Asp-Net45"
         }
 
         # Stop the default website
         xWebsite DefaultSite
         {
-            Ensure          = 'Present'
-            Name            = 'Default Web Site'
-            State           = 'Stopped'
-            ServerAutoStart = $false
-            PhysicalPath    = 'C:\inetpub\wwwroot'
-            DependsOn       = '[WindowsFeature]IIS'
+            Ensure          = "Present"
+            Name            = "Default Web Site"
+            State           = "Stopped"
+            PhysicalPath    = "C:\inetpub\wwwroot"
+            DependsOn       = "[WindowsFeature]IIS"
         }
 
         # Copy the website content
         File WebContent
         {
-            Ensure          = 'Present'
+            Ensure          = "Present"
             SourcePath      = $SourcePath
             DestinationPath = $DestinationPath
             Recurse         = $true
-            Type            = 'Directory'
-            DependsOn       = '[WindowsFeature]AspNet45'
+            Type            = "Directory"
+            DependsOn       = "[WindowsFeature]AspNet45"
         }
 
-        # Create the new Website
+        # Create the new Website with HTTPS
         xWebsite NewWebsite
         {
-            Ensure          = 'Present'
+            Ensure          = "Present"
             Name            = $WebSiteName
-            SiteId       = $SiteId
-            State           = 'Started'
-            ServerAutoStart = $true
+            State           = "Started"
             PhysicalPath    = $DestinationPath
-            DependsOn       = '[File]WebContent'
+            BindingInfo     = @(
+                MSFT_xWebBindingInformation
+                {
+                    Protocol              = "HTTPS"
+                    Port                  = 8444
+                    CertificateSubject    = "CN=CertificateSubject"
+                    CertificateStoreName  = "MY"
+                }
+            )
+            DependsOn       = "[File]WebContent"
         }
     }
 }
-
