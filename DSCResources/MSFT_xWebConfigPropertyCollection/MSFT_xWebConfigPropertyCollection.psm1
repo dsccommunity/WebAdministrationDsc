6334d523ee53c179c68193ca8d893d0d19ee4345
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2019-10-07 20:08:55 +0200

    Corrected MSFT_xWebConfigPropertyCollection.psm1 to adhere to style guidelines ref #524

--- DSCResources/MSFT_xWebConfigPropertyCollection/MSFT_xWebConfigPropertyCollection.psm1
+++ DSCResources/MSFT_xWebConfigPropertyCollection/MSFT_xWebConfigPropertyCollection.psm1
@@ -251,7 +251,10 @@ function Set-TargetResource
                 -PSPath $WebsitePath `
                 -Filter $filter `
                 -Name '.' `
-                -Value $(@{$ItemKeyName=$ItemKeyValue;$ItemPropertyName=$setItemPropertyValue})
+                -Value @{
+                    $ItemKeyName = $ItemKeyValue
+                    $ItemPropertyName = $setItemPropertyValue
+                }
         }
         else
         {
@@ -279,7 +282,9 @@ function Set-TargetResource
             -PSPath $WebsitePath `
             -Filter $filter `
             -Name '.' `
-            -AtElement @{$ItemKeyName=$ItemKeyValue}
+            -AtElement @{
+                $ItemKeyName = $ItemKeyValue
+            }
     }
 }
 
