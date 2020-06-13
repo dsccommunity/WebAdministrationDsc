a8c3dd3b34d6fc21b1cb842917fa006bd124e815
ThomasHughesIV <42014035+ThomasHughesIV@users.noreply.github.com>
2020-06-13 12:32:14 -0700

    Esnured private  key is there, and cert > NotAfter

--- source/DSCResources/MSFT_xWebSite/MSFT_xWebSite.psm1
+++ source/DSCResources/MSFT_xWebSite/MSFT_xWebSite.psm1
@@ -1473,7 +1473,7 @@ function ConvertTo-WebBinding
                     if ($FindCertificateSplat)
                     {
                         $FindCertificateSplat.Add('Store',$CertificateStoreName)
-                        $Certificate = Find-Certificate @FindCertificateSplat | Sort-Object -Property NotAfter -Descending | Select-Object -First 1
+                        $Certificate = Find-Certificate @FindCertificateSplat | Where-Object {$_.PrivateKey -ne ''} | Sort-Object -Property NotAfter -Descending | Select-Object -First 1
                         if ($Certificate)
                         {
                             $certificateHash = $Certificate.Thumbprint
