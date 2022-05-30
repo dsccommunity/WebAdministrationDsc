c93dd6d8a2253d36d4c165effaf7d90b0524eefd
Nick Germany <nickgw@outlook.com>
2022-05-30 13:55:36 -0400

    attempting a sleep before get

--- tests/Integration/DSC_WebApplication.Integration.Tests.ps1
+++ tests/Integration/DSC_WebApplication.Integration.Tests.ps1
@@ -91,15 +91,11 @@ try
             } | Should -Not -Throw
         }
 
-        # It 'Should compile without throwing' {
-        #     {
-        #         Invoke-Expression -Command "$($script:dscResourceName)_Present -ConfigurationData `$configData -OutputPath `$TestDrive"
-        #         Start-DscConfiguration -Path $TestDrive -ComputerName localhost -Wait -Verbose -Force
-        #     } | Should not throw
-        # }
-
         It 'Should be able to call Get-DscConfiguration without throwing' {
-            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+            {
+                Start-Sleep -Seconds 5
+                Get-DscConfiguration -Verbose -ErrorAction Stop
+            } | Should -Not -Throw
         }
         #endregion
 
